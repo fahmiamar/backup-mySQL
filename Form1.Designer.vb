584199a0fd95280b7272d0fe029e32f03f799c4a
unknown <fahmi592693@gmail.com>
2024-07-21 19:46:12 +0700

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.BtnKoneksi = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TxtPassword = New System.Windows.Forms.TextBox()
+        Me.TxtUser = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TxtServer = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnRestore = New System.Windows.Forms.Button()
+        Me.btnBackup = New System.Windows.Forms.Button()
+        Me.cboDatabase = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(72, 40)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 33)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Keluar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.BtnKoneksi)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.TxtPassword)
+        Me.GroupBox1.Controls.Add(Me.TxtUser)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TxtServer)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(81, 93)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(634, 257)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Tes Koneksi"
+        '
+        'BtnKoneksi
+        '
+        Me.BtnKoneksi.Location = New System.Drawing.Point(111, 180)
+        Me.BtnKoneksi.Name = "BtnKoneksi"
+        Me.BtnKoneksi.Size = New System.Drawing.Size(227, 33)
+        Me.BtnKoneksi.TabIndex = 6
+        Me.BtnKoneksi.Text = "Tes Koneksi"
+        Me.BtnKoneksi.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 134)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 17)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Password"
+        '
+        'TxtPassword
+        '
+        Me.TxtPassword.Location = New System.Drawing.Point(111, 129)
+        Me.TxtPassword.Name = "TxtPassword"
+        Me.TxtPassword.Size = New System.Drawing.Size(227, 22)
+        Me.TxtPassword.TabIndex = 4
+        '
+        'TxtUser
+        '
+        Me.TxtUser.Location = New System.Drawing.Point(111, 90)
+        Me.TxtUser.Name = "TxtUser"
+        Me.TxtUser.Size = New System.Drawing.Size(227, 22)
+        Me.TxtUser.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 90)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "User ID"
+        '
+        'TxtServer
+        '
+        Me.TxtServer.Location = New System.Drawing.Point(111, 49)
+        Me.TxtServer.Name = "TxtServer"
+        Me.TxtServer.Size = New System.Drawing.Size(227, 22)
+        Me.TxtServer.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Server"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnRestore)
+        Me.GroupBox2.Controls.Add(Me.btnBackup)
+        Me.GroupBox2.Controls.Add(Me.cboDatabase)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Location = New System.Drawing.Point(81, 374)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(634, 214)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Database"
+        '
+        'btnRestore
+        '
+        Me.btnRestore.Location = New System.Drawing.Point(111, 151)
+        Me.btnRestore.Name = "btnRestore"
+        Me.btnRestore.Size = New System.Drawing.Size(227, 33)
+        Me.btnRestore.TabIndex = 8
+        Me.btnRestore.Text = "Restore Database"
+        Me.btnRestore.UseVisualStyleBackColor = True
+        '
+        'btnBackup
+        '
+        Me.btnBackup.Location = New System.Drawing.Point(111, 101)
+        Me.btnBackup.Name = "btnBackup"
+        Me.btnBackup.Size = New System.Drawing.Size(227, 33)
+        Me.btnBackup.TabIndex = 7
+        Me.btnBackup.Text = "Backup Database"
+        Me.btnBackup.UseVisualStyleBackColor = True
+        '
+        'cboDatabase
+        '
+        Me.cboDatabase.FormattingEnabled = True
+        Me.cboDatabase.Location = New System.Drawing.Point(111, 37)
+        Me.cboDatabase.Name = "cboDatabase"
+        Me.cboDatabase.Size = New System.Drawing.Size(227, 24)
+        Me.cboDatabase.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 40)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(69, 17)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Database"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 624)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents BtnKoneksi As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TxtPassword As TextBox
+    Friend WithEvents TxtUser As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TxtServer As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnRestore As Button
+    Friend WithEvents btnBackup As Button
+    Friend WithEvents cboDatabase As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ Form1.vb
@@ -0,0 +1,117 @@
+﻿Imports mySql.Data.MySqlClient
+Imports System.IO
+Public Class Form1
+    Dim Conn As MySqlConnection
+    Dim dt As New DataTable
+    Dim da As MySqlDataAdapter
+    Dim cmd As String
+    Dim i As Integer
+
+    Public Sub KoneksiDB()
+        Try
+            Conn = New MySqlConnection("Server=" & TxtServer.Text & ";user id=" & TxtUser.Text & ";password=" & TxtPassword.Text & ";")
+            If Conn.State = ConnectionState.Closed Then
+                Conn.Open()
+                cboDatabase.Enabled = True
+            End If
+        Catch ex As Exception
+            MsgBox("Koneksi gagal ..... !", MsgBoxStyle.Critical, "Gagal Koneksi !")
+        End Try
+    End Sub
+
+    Private Sub BtnKoneksi_Click(sender As Object, e As EventArgs) Handles BtnKoneksi.Click
+        Try
+            Call KoneksiDB()
+            cmd = "SELECT DISTINCT TABLE_SCHEMA FROM information_schema.TABLES"
+            da = New MySqlDataAdapter(cmd, Conn)
+            da.Fill(dt)
+            i = 0
+            cboDatabase.Enabled = True
+            cboDatabase.Items.Clear()
+            cboDatabase.Items.Add("+++ Select Database +++")
+            While i < dt.Rows.Count
+                cboDatabase.Items.Add(dt.Rows(i)(0).ToString())
+                i = i + 1
+
+            End While
+            cboDatabase.SelectedIndex = 0
+            BtnKoneksi.Enabled = False
+            btnBackup.Enabled = True
+            btnRestore.Enabled = True
+            Conn.Clone()
+            dt.Dispose()
+            da.Dispose()
+            MsgBox("Koneksi database berhasil dilakukan", MsgBoxStyle.Information, "Informasi")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        cboDatabase.Enabled = False
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnBackup_Click(sender As Object, e As EventArgs) Handles btnBackup.Click
+        Dim dbFile As String
+        Try
+            SaveFileDialog1.Filter = "SQL Dump File(*.sql|*.sql|All files(*.*)|*.*"
+            SaveFileDialog1.FileName = "Backup " + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".sql"
+            If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+                Call KoneksiDB()
+                dbFile = SaveFileDialog1.FileName
+                Dim BackuProses As New Process
+                BackuProses.StartInfo.FileName = "cmd.exe"
+                BackuProses.StartInfo.UseShellExecute = False
+                BackuProses.StartInfo.WorkingDirectory = "C:\xampp\mysql\bin\"
+                BackuProses.StartInfo.RedirectStandardInput = True
+                BackuProses.StartInfo.RedirectStandardOutput = True
+                BackuProses.Start()
+                Dim BackupStream As StreamWriter = BackuProses.StandardInput
+                Dim MyStreamReader As StreamReader = BackuProses.StandardOutput
+                BackupStream.WriteLine("mysqldump --routines --user=" & TxtUser.Text & " --password=" & TxtPassword.Text & " -h " & TxtServer.Text & " " & cboDatabase.Text & ">""" + dbFile + """")
+                BackupStream.Close()
+                BackuProses.WaitForExit()
+                BackuProses.Close()
+                Conn.Close()
+                MsgBox("Backup data mySQL telah berhasil dilakukan !", MsgBoxStyle.Information, "Backup databasse  MySQL")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnRestore_Click(sender As Object, e As EventArgs) Handles btnRestore.Click
+        Dim dbFile As String
+        Try
+            OpenFileDialog1.Filter = "SQL Dump File(*.sql|*.sql|All files(*.*)|*.*"
+
+            If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+                Call KoneksiDB()
+                dbFile = OpenFileDialog1.FileName
+                Dim BackuProses As New Process
+                BackuProses.StartInfo.FileName = "cmd.exe"
+                BackuProses.StartInfo.UseShellExecute = False
+                BackuProses.StartInfo.WorkingDirectory = "C:\xampp\mysql\bin\"
+                BackuProses.StartInfo.RedirectStandardInput = True
+                BackuProses.StartInfo.RedirectStandardOutput = True
+                BackuProses.StartInfo.CreateNoWindow = True
+                BackuProses.Start()
+                Dim BackupStream As StreamWriter = BackuProses.StandardInput
+                Dim MyStreamReader As StreamReader = BackuProses.StandardOutput
+                BackupStream.WriteLine("mysql  --user=" & TxtUser.Text & " --password=" & TxtPassword.Text & " -h " & TxtServer.Text & " " & cboDatabase.Text & "<""" + dbFile + """")
+                BackupStream.Close()
+                BackuProses.WaitForExit()
+                BackuProses.Close()
+                Conn.Close()
+                MsgBox("Restore your Mysql Database Created Succesfully !", MsgBoxStyle.Information, "Restore MySQL Database")
+
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
